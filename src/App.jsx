d1ac9a370d684bbb21566e2b1b96fd127945d97a
lucia-caranfa <lucia.caranfa@contabilium.com>
2023-08-17 16:11:02 -0300

    Material UI 01 17/8

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import MaterialUI from './components/MaterialUI'
+
+
+function App() {
+
+  return (
+    <>
+<h2 className='texto'>Hola!</h2>
+
+<MaterialUI/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/MaterialUI.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MediaCard from './MaterialCard'
+import TemporaryDrawer from './MaterialDrawer'
+
+
+function MaterialUI() {
+  return (
+    <>
+    <h2 className='texto'>MaterialUI</h2>
+    <MediaCard/>
+    <TemporaryDrawer/>
+    </>
+  )
+}
+
+export default MaterialUI
